83d8d8169ddb2679f4e66f4e6db027259fc7df16
shashank chourasia01 <shashankchourasia0829@gmail.com>
2025-08-19 14:56:51 +0530

    qf

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
   <div>
-    <h2>
-      shashank <br />
-      <Button variant="destructive">Button</Button>
+    <h2 className="text-2xl text-blue-700 font-bold text-center mt-10">
+      This is AI-Mock Interview Platform <br />
+      {/* <Button className="font-bold text-blue-700">Button</Button> */}
+    </h2>
+    <h2 className="text-2xl text-blue-700 font-bold text-center mt-10">
+      Go to Dashboard Page to start the interview process
     </h2>
   </div>
   );
